1b23d79503b182a0812ab5ce882392efa24936a0
Chameleon Cloud User <cc@envagentplus2.novalocal>
2026-04-15 22:20:37 +0000

    Added new changes

---
+++ latex.py
@@ -0,0 +1,65 @@
+repos = [
+"RelTR",
+"Lottery-Ticket-Hypothesis-in-Pytorch",
+"gnn_editing",
+"TabPFN",
+"neural-decoding-RSNN",
+"p4control",
+"crossprefetch-asplos24-artifacts",
+"SymMC-Tool",
+"Fairify",
+"exli",
+"sixthsense",
+"probfuzz",
+"gluetest",
+"flex",
+"acto",
+"Baleen-FAST24",
+"Silhouette",
+"anvil",
+"ELECT",
+"rfuse",
+"Metis",
+"zstd",
+"jq",
+"ponyc",
+"Catch2",
+"fmt",
+"json",
+"simdjson",
+"cpp-httplib",
+"cli",
+"grpc-go",
+"go-zero",
+"fastjson2",
+"logstash",
+"mockito",
+"github-readme-stats",
+"axios",
+"express",
+"dayjs",
+"insomnia",
+"svelte",
+"ripgrep",
+"clap",
+"nushell",
+"serde",
+"bat",
+"fd",
+"rayon",
+"bytes",
+"tokio",
+"tracing",
+"darkreader",
+"material-ui",
+"core"
+]
+
+methods = ["Ours", "Claude Code", "Codex"]
+models = ["GPT-4.1-mini", "TBD-1"]
+
+for repo in repos:
+    for method in methods:
+        for model in models:
+            print(f"{repo} & {method} & {model} &  &  &  \\\\")
+    print("\\midrule")
